432131b5984a71973f42bcb7624048ed53ec5361
Akhil Jose <dev.akhiljoseko@gmail.com>
2023-11-05 23:50:48 +0530

    Added new icons
    - person
    - work
    - drop down arrow
    - arrow right
    - calendar

--- lib/app/app_icons.dart
+++ lib/app/app_icons.dart
@@ -1,5 +1,12 @@
+// ignore_for_file: constant_identifier_names
+
 class AppIcons {
   static const _location = "assets/icons/";
 
   static const add = "${_location}add.svg";
+  static const person_outlined = "${_location}person_outlined.svg";
+  static const work_outlined = "${_location}work_outlined.svg";
+  static const calendar_outlined = "${_location}calendar_outlined.svg";
+  static const arrow_right = "${_location}arrow_right.svg";
+  static const arrow_drop_down = "${_location}arrow_drop_down.svg";
 }
