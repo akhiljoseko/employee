bfae646856ea98e1efa5585047fd07e6704ad49a
Akhil Jose <dev.akhiljoseko@gmail.com>
2023-11-05 22:51:28 +0530

    register route to new screen

--- lib/app/app_router.dart
+++ lib/app/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:employee/app/route_names.dart';
+import 'package:employee/view/screens/add_edit_employee_screen.dart';
 import 'package:employee/view/screens/employee_list_screen.dart';
 import 'package:go_router/go_router.dart';
 
@@ -9,7 +10,14 @@ class AppRouter {
         name: RouteNames.employeeListScreen,
         path: "/",
         builder: (context, state) => const EmployeeListScreen(),
-      )
+        routes: [
+          GoRoute(
+            name: RouteNames.addEmployee,
+            path: "add",
+            builder: (context, state) => const AddEditEmployeeScreen(),
+          ),
+        ],
+      ),
     ],
   );
 }
--- lib/app/route_names.dart
+++ lib/app/route_names.dart
@@ -1,3 +1,4 @@
 class RouteNames {
   static const employeeListScreen = 'employee-list';
+  static const addEmployee = 'add-employee';
 }
