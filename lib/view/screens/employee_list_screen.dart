5088aba87cae2902197dc7f7c641e96bd7303019
Akhil Jose <dev.akhiljoseko@gmail.com>
2023-11-05 19:02:13 +0530

    list view ui and heading

--- lib/view/screens/employee_list_screen.dart
+++ lib/view/screens/employee_list_screen.dart
@@ -1,16 +1,60 @@
+import 'package:employee/app/app_theme.dart';
+import 'package:employee/view/providers/employees_list_provider.dart';
 import 'package:employee/view/widgets/empty_list_info.dart';
+import 'package:employee/view/widgets/list_heading_text.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class EmployeeListScreen extends StatelessWidget {
   const EmployeeListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Employee List"),
+    return ChangeNotifierProvider(
+      create: (context) => EmployeesListProvider(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Employee List"),
+        ),
+        body: Consumer<EmployeesListProvider>(
+          builder: (context, provider, child) {
+            //TODO: Add error info if needed
+            if (provider.isEmployeesListEmpty) {
+              return const EmptyListInfo();
+            }
+            return CustomScrollView(
+              slivers: [
+                SliverList.builder(
+                  itemCount: 5 + 1,
+                  itemBuilder: (context, index) {
+                    if (index == 0) {
+                      return const HeadingText(
+                        headingText: "Current employees",
+                      );
+                    }
+                    return const ListTile(
+                      title: Text("Test"),
+                    );
+                  },
+                ),
+                SliverList.builder(
+                  itemCount: 5 + 1,
+                  itemBuilder: (context, index) {
+                    if (index == 0) {
+                      return const HeadingText(
+                        headingText: "Previous employees",
+                      );
+                    }
+                    return const ListTile(
+                      title: Text("Test"),
+                    );
+                  },
+                ),
+              ],
+            );
+          },
+        ),
       ),
-      body: const EmptyListInfo(),
     );
   }
 }
