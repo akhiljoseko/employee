b41a8a80f194786b10e42da3484f5ad9aea3ea16
Akhil Jose <dev.akhiljoseko@gmail.com>
2023-11-05 23:51:15 +0530

    add employee screen ui

--- lib/view/screens/add_edit_employee_screen.dart
+++ lib/view/screens/add_edit_employee_screen.dart
@@ -1,4 +1,7 @@
+import 'package:employee/app/app_icons.dart';
+import 'package:employee/view/widgets/gap.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class AddEditEmployeeScreen extends StatelessWidget {
   const AddEditEmployeeScreen({super.key});
@@ -10,16 +13,101 @@ class AddEditEmployeeScreen extends StatelessWidget {
         title: const Text("Employee List"),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            TextFormField(
-              decoration: const InputDecoration(
-                label: Text("Employee name"),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Column(
+            children: [
+              const VGap(16),
+              TextFormField(
+                decoration: InputDecoration(
+                  label: const Text("Employee name"),
+                  prefixIcon: SvgPicture.asset(
+                    AppIcons.person_outlined,
+                    width: 24,
+                    height: 24,
+                    fit: BoxFit.contain,
+                  ),
+                  prefixIconConstraints: BoxConstraints.tight(
+                    const Size(48, 24),
+                  ),
+                ),
               ),
-            )
-          ],
+              const VGap(18),
+              TextFormField(
+                canRequestFocus: false,
+                onTap: _openRoleDropDown,
+                decoration: InputDecoration(
+                  label: const Text("Select role"),
+                  prefixIcon: SvgPicture.asset(
+                    AppIcons.work_outlined,
+                    width: 24,
+                    height: 24,
+                    fit: BoxFit.contain,
+                  ),
+                  prefixIconConstraints: BoxConstraints.tight(
+                    const Size(48, 24),
+                  ),
+                  suffixIcon: SvgPicture.asset(
+                    AppIcons.arrow_drop_down,
+                    width: 24,
+                    height: 24,
+                    fit: BoxFit.contain,
+                  ),
+                  suffixIconConstraints: BoxConstraints.tight(
+                    const Size(48, 24),
+                  ),
+                ),
+              ),
+              const VGap(18),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        label: const Text("Today"),
+                        prefixIcon: SvgPicture.asset(
+                          AppIcons.calendar_outlined,
+                          width: 24,
+                          height: 24,
+                          fit: BoxFit.contain,
+                        ),
+                        prefixIconConstraints: BoxConstraints.tight(
+                          const Size(48, 24),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12),
+                    child: SvgPicture.asset(AppIcons.arrow_right),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        label: const Text("No Date"),
+                        prefixIcon: SvgPicture.asset(
+                          AppIcons.calendar_outlined,
+                          width: 24,
+                          height: 24,
+                          fit: BoxFit.contain,
+                        ),
+                        prefixIconConstraints: BoxConstraints.tight(
+                          const Size(48, 24),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void _openRoleDropDown() {
+    //TODO: Role drop down
+    print("Role drop down");
   }
 }
