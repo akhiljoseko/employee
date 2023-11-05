c70a807033f09c09eea4d90e70ab661335a82a30
Akhil Jose <dev.akhiljoseko@gmail.com>
2023-11-05 18:12:59 +0530

    widget to show list empty info

---
+++ lib/view/widgets/empty_list_info.dart
@@ -0,0 +1,31 @@
+import 'package:employee/app/app_images.dart';
+import 'package:employee/view/widgets/gap.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class EmptyListInfo extends StatelessWidget {
+  const EmptyListInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            AppImages.empty_list_illustration,
+            width: 260,
+            height: 240,
+            fit: BoxFit.fill,
+          ),
+          const VGap(8),
+          Text(
+            "No employee records found",
+            style: Theme.of(context).textTheme.bodyLarge,
+          )
+        ],
+      ),
+    );
+  }
+}
