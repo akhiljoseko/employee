170337f68c66e54e1bb2e092df115966d65685c8
Akhil Jose <77378123+akhiljoseko@users.noreply.github.com>
2023-11-09 00:48:52 +0530

    Update role.dart

--- lib/domain/models/role.dart
+++ lib/domain/models/role.dart
@@ -7,4 +7,11 @@ class Role extends Equatable {
   const Role({required this.id, required this.role});
   @override
   List<Object?> get props => [id, role];
+
+  static const roles = [
+    Role(id: 1, role: "Product Designer"),
+    Role(id: 2, role: "Flutter Developer"),
+    Role(id: 3, role: "QA Tester"),
+    Role(id: 4, role: "Product Owner"),
+  ];
 }
