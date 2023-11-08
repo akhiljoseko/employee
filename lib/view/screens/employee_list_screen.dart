3e831cd0251cfa3a8ecf12f38ca2f5455c822f67
Akhil Jose <77378123+akhiljoseko@users.noreply.github.com>
2023-11-08 20:13:46 +0530

    Update employee_list_screen.dart

--- lib/view/screens/employee_list_screen.dart
+++ lib/view/screens/employee_list_screen.dart
@@ -1,12 +1,14 @@
-import 'dart:math';
-
 import 'package:employee/app/app_icons.dart';
 import 'package:employee/app/app_theme.dart';
 import 'package:employee/app/route_names.dart';
+import 'package:employee/domain/employee_repository.dart';
 import 'package:employee/domain/models/employee.dart';
+import 'package:employee/view/cubits/cubit/employees_list_cubit.dart';
 import 'package:employee/view/widgets/employee_list_tile.dart';
+import 'package:employee/view/widgets/empty_list_info.dart';
 import 'package:employee/view/widgets/list_heading_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 
@@ -15,82 +17,104 @@ class EmployeeListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Employee List"),
+    return BlocProvider<EmployeesListCubit>(
+      create: (context) => EmployeesListCubit(
+        employeeRepository: context.read<EmployeeRepository>(),
       ),
-      floatingActionButton: FloatingActionButton(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(12),
-          ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Employee List"),
         ),
-        onPressed: () => _addEmployee(context),
-        child: SvgPicture.asset(AppIcons.add),
-      ),
-      body: CustomScrollView(
-        slivers: [
-          SliverList.builder(
-            itemCount: 7 + 1,
-            itemBuilder: (context, index) {
-              if (index == 0) {
-                return const HeadingText(
-                  headingText: "Current employees",
-                );
-              }
-              final employee = Employee(
-                id: Random().nextInt(500),
-                name: "Samantha Lee",
-                roleId: 2,
-                fromDate: DateTime(2023, 10, 23),
-              );
-              return EmployeeListTile(
-                key: ValueKey<int>(employee.id!),
-                employee: employee,
-                isExpiredEmployee: false,
-                onDeleted: _deleteEmployee,
-              );
-            },
-          ),
-          SliverList.builder(
-            itemCount: 5 + 1,
-            itemBuilder: (context, index) {
-              if (index == 0) {
-                return const HeadingText(
-                  headingText: "Previous employees",
-                );
-              }
-              final employee = Employee(
-                id: Random().nextInt(500),
-                name: "James Lee",
-                roleId: 2,
-                fromDate: DateTime(2022, 8, 1),
-                toDate: DateTime(2023, 10, 30),
-              );
-              return EmployeeListTile(
-                key: ValueKey<int>(employee.id!),
-                employee: employee,
-                isExpiredEmployee: true,
-                onDeleted: _deleteEmployee,
-              );
-            },
-          ),
-          SliverToBoxAdapter(
-            child: DecoratedBox(
-              decoration: const BoxDecoration(
-                color: AppColors.grey,
-              ),
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 24, horizontal: 12),
-                child: Text(
-                  "Swipe left to delete",
-                  style: Theme.of(context).textTheme.labelLarge,
-                ),
-              ),
+        floatingActionButton: FloatingActionButton(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(12),
             ),
-          )
-        ],
+          ),
+          onPressed: () => _addEmployee(context),
+          child: SvgPicture.asset(AppIcons.add),
+        ),
+        body: BlocBuilder<EmployeesListCubit, EmployeesListState>(
+          builder: (context, state) {
+            if (state.status == EmployeesListStateStatus.initial ||
+                state.status == EmployeesListStateStatus.loading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (state.status == EmployeesListStateStatus.failure) {
+              return Center(
+                child: Text(
+                    "Sorry!\nAn error occured when fetching employees\n${state.error}"),
+              );
+            }
+            if (state.status == EmployeesListStateStatus.success &&
+                state.employees.isEmpty) {
+              return const EmptyListInfo();
+            }
+            final expiredEmployees = state.employees
+                .where((emp) => emp.toDate?.isBefore(DateTime.now()) ?? false)
+                .toList();
+            final currentEmployees = state.employees
+                .where((emp) => emp.toDate?.isAfter(DateTime.now()) ?? true)
+                .toList();
+            return CustomScrollView(
+              slivers: [
+                if (currentEmployees.isNotEmpty)
+                  SliverList.builder(
+                    itemCount: currentEmployees.length + 1,
+                    itemBuilder: (context, index) {
+                      if (index == 0) {
+                        return const HeadingText(
+                          headingText: "Current employees",
+                        );
+                      }
+                      final employee = currentEmployees[index - 1];
+                      return EmployeeListTile(
+                        key: ValueKey<int>(employee.id!),
+                        employee: employee,
+                        isExpiredEmployee: false,
+                        onDeleted: () => _deleteEmployee(context, employee),
+                      );
+                    },
+                  ),
+                if (expiredEmployees.isNotEmpty)
+                  SliverList.builder(
+                    itemCount: expiredEmployees.length + 1,
+                    itemBuilder: (context, index) {
+                      if (index == 0) {
+                        return const HeadingText(
+                          headingText: "Previous employees",
+                        );
+                      }
+                      final employee = currentEmployees[index - 1];
+                      return EmployeeListTile(
+                        key: ValueKey<int>(employee.id!),
+                        employee: employee,
+                        isExpiredEmployee: true,
+                        onDeleted: () => _deleteEmployee(context, employee),
+                      );
+                    },
+                  ),
+                SliverToBoxAdapter(
+                  child: DecoratedBox(
+                    decoration: const BoxDecoration(
+                      color: AppColors.grey,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 24, horizontal: 12),
+                      child: Text(
+                        "Swipe left to delete",
+                        style: Theme.of(context).textTheme.labelLarge,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            );
+          },
+        ),
       ),
     );
   }
@@ -99,7 +123,21 @@ class EmployeeListScreen extends StatelessWidget {
     context.goNamed(RouteNames.addEmployee);
   }
 
-  void _deleteEmployee() {
-    //TODO: Delete employee
+  void _deleteEmployee(BuildContext context, Employee deletedEmployee) {
+    print("Deleted");
+    context.read<EmployeesListCubit>().deleteEmployee(deletedEmployee);
+    final snackbar = SnackBar(
+      content: Text("Employee data as been deleted"),
+      action: SnackBarAction(
+        label: "Undo",
+        onPressed: () {
+          context
+              .read<EmployeesListCubit>()
+              .undoEmployeeDeletion(deletedEmployee);
+        },
+      ),
+    );
+    ScaffoldMessenger.of(context).removeCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(snackbar);
   }
 }
