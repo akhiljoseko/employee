e093710926e39b193cf95056088589d353f88bc1
Akhil Jose <77378123+akhiljoseko@users.noreply.github.com>
2023-11-08 23:10:56 +0530

    Update employee_repository_impl.dart

--- lib/domain/employee_repository_impl.dart
+++ lib/domain/employee_repository_impl.dart
@@ -1,11 +1,8 @@
+import 'dart:async';
 import 'dart:math';
-
 import 'package:dartz/dartz.dart';
-
 import 'package:employee/app/errors.dart';
-
 import 'package:employee/domain/models/employee.dart';
-
 import 'employee_repository.dart';
 
 class EmployeeRepositoryImpl implements EmployeeRepository {
@@ -17,19 +14,26 @@ class EmployeeRepositoryImpl implements EmployeeRepository {
           id: Random().nextInt(30),
           name: "Test",
           roleId: 2,
-          fromDate: DateTime(2022, 10, 01))
+          fromDate: DateTime(2022, 10, 01)),
+      Employee(
+        id: Random().nextInt(30),
+        name: "Test2",
+        roleId: 2,
+        fromDate: DateTime(2022, 10, 01),
+        toDate: DateTime(2022, 10, 01),
+      )
     ]);
   }
 
   @override
   Future<Either<CustomException, bool>> deleteEmployee(
       Employee deletedEmployee) async {
-    return Right(true);
+    return const Right(true);
   }
 
   @override
   Future<Either<CustomException, bool>> undoEmployeeDeletion(
       Employee? lastDeletedEmployee) async {
-    return Right(true);
+    return const Right(true);
   }
 }
