52a31f322a9023fbde05256a176ea8671828786f
Akhil Jose <77378123+akhiljoseko@users.noreply.github.com>
2023-11-08 23:11:00 +0530

    Update employee_app.dart

--- lib/app/employee_app.dart
+++ lib/app/employee_app.dart
@@ -1,15 +1,24 @@
 import 'package:employee/app/app_router.dart';
 import 'package:employee/app/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../domain/employee_repository.dart';
+import '../view/cubits/cubit/employees_list_cubit.dart';
 
 class EmployeeApp extends StatelessWidget {
   const EmployeeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      theme: AppTheme().lightTheme,
-      routerConfig: AppRouter.router,
+    return BlocProvider<EmployeesListCubit>(
+      create: (context) => EmployeesListCubit(
+        employeeRepository: context.read<EmployeeRepository>(),
+      ),
+      child: MaterialApp.router(
+        theme: AppTheme().lightTheme,
+        routerConfig: AppRouter.router,
+      ),
     );
   }
 }
