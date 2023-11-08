8391f23d6c90731d966c9d9c99bd3ec216a57584
Akhil Jose <77378123+akhiljoseko@users.noreply.github.com>
2023-11-08 23:10:02 +0530

    Update employees_list_state.dart

--- lib/view/cubits/cubit/employees_list_state.dart
+++ lib/view/cubits/cubit/employees_list_state.dart
@@ -1,12 +1,8 @@
 part of 'employees_list_cubit.dart';
 
-enum EmployeesListStateStatus {
-  initial,
-  loading,
-  success,
-  failure,
-  employeeDeleted
-}
+enum EmployeesListStateStatus { initial, loading, success, failure }
+
+enum EmployeeListOperation { none, deleted, undo }
 
 class EmployeesListState extends Equatable {
   const EmployeesListState({
@@ -14,27 +10,32 @@ class EmployeesListState extends Equatable {
     this.lastDeletedEmployee,
     this.employees = const [],
     this.error = "",
+    this.operation = EmployeeListOperation.none,
   });
 
   final EmployeesListStateStatus status;
   final List<Employee> employees;
   final Employee? lastDeletedEmployee;
   final String error;
+  final EmployeeListOperation operation;
 
   EmployeesListState copyWith({
     EmployeesListStateStatus? status,
     List<Employee>? employees,
     Employee? lastDeletedEmployee,
     String? error,
+    EmployeeListOperation? operation,
   }) {
     return EmployeesListState(
       status: status ?? this.status,
       employees: employees ?? this.employees,
       lastDeletedEmployee: lastDeletedEmployee ?? this.lastDeletedEmployee,
       error: error ?? this.error,
+      operation: operation ?? this.operation,
     );
   }
 
   @override
-  List<Object?> get props => [status, employees, lastDeletedEmployee, error];
+  List<Object?> get props =>
+      [status, employees, lastDeletedEmployee, error, operation];
 }
