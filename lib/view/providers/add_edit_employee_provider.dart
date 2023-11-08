c0f3bbef35c3e21d1bbba07396b342a82d8c4b8b
Akhil Jose <77378123+akhiljoseko@users.noreply.github.com>
2023-11-09 00:49:08 +0530

    save function

--- lib/view/providers/add_edit_employee_provider.dart
+++ lib/view/providers/add_edit_employee_provider.dart
@@ -1,8 +1,27 @@
 import 'package:employee/app/base_provider.dart';
+// ignore: depend_on_referenced_packages
+import 'package:collection/collection.dart';
+import 'package:employee/domain/employee_repository.dart';
 import 'package:employee/domain/models/employee.dart';
 import 'package:employee/domain/models/role.dart';
+import 'package:employee/view/screen_args/add_edit_employee_screen_args.dart';
 
 class AddEditEmployeeProvider extends BaseProvider {
+  final AddEditEmployeeScreenArgs? args;
+  final EmployeeRepository employeeRepository;
+
+  AddEditEmployeeProvider({
+    required this.args,
+    required this.employeeRepository,
+  }) {
+    _isAdding = args?.id == null;
+    if (!_isAdding) {
+      _setVariblesWithData();
+    }
+  }
+
+  late bool _isAdding;
+
   String? name;
   void onNameChanged(String value) {
     name = value;
@@ -46,12 +65,23 @@ class AddEditEmployeeProvider extends BaseProvider {
 
   Future<bool> save() async {
     final employee = Employee(
+      id: args?.id,
       name: name!,
       roleId: selectedRole!.id,
       fromDate: fromDate!,
       toDate: toDate,
     );
-    print(employee);
-    return false;
+    busy();
+    final result = await employeeRepository.addOrUpdateEmployee(employee);
+    idle();
+    return result.fold((error) => false, (isAdded) => isAdded);
+  }
+
+  void _setVariblesWithData() {
+    name = args?.name;
+    selectedRole =
+        Role.roles.firstWhereOrNull((element) => element.id == args?.roleId);
+    fromDate = args?.fromDate;
+    toDate = args?.toDate;
   }
 }
