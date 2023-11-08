f897b7ee7ea75784532a6705ae6f6df0a2a7a3ba
Akhil Jose <77378123+akhiljoseko@users.noreply.github.com>
2023-11-09 02:33:42 +0530

    Update main.dart
    - initialising db and providing instance in employee repo

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:employee/database/isar_employee_api.dart';
 import 'package:employee/domain/employee_repository.dart';
 import 'package:employee/domain/employee_repository_impl.dart';
 import 'package:flutter/material.dart';
@@ -5,12 +6,18 @@ import 'package:provider/provider.dart';
 
 import 'app/employee_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await IsarEmployeeApi.initApi();
+  final isarApi = IsarEmployeeApi();
+
   runApp(
     MultiProvider(
       providers: [
         Provider<EmployeeRepository>(
-          create: (context) => EmployeeRepositoryImpl(),
+          create: (context) => EmployeeRepositoryImpl(
+            employeeApi: isarApi,
+          ),
         )
       ],
       child: const EmployeeApp(),
