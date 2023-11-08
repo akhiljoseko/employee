6419cc31d0dcaa5df90a04e4076909ba0f3cd97f
Akhil Jose <77378123+akhiljoseko@users.noreply.github.com>
2023-11-08 23:09:43 +0530

    Update employee_list_tile.dart

--- lib/view/widgets/employee_list_tile.dart
+++ lib/view/widgets/employee_list_tile.dart
@@ -1,10 +1,7 @@
-import 'package:employee/app/route_names.dart';
 import 'package:employee/domain/models/employee.dart';
 import 'package:employee/utils/date_time_utils.dart';
-import 'package:employee/view/screen_args/add_edit_employee_screen_args.dart';
 import 'package:employee/view/widgets/space.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 class EmployeeListTile extends StatelessWidget {
   const EmployeeListTile({
@@ -12,11 +9,13 @@ class EmployeeListTile extends StatelessWidget {
     required this.isExpiredEmployee,
     required this.onDeleted,
     required this.employee,
+    required this.onEditPressed,
   });
 
   final Employee employee;
   final bool isExpiredEmployee;
   final VoidCallback onDeleted;
+  final VoidCallback onEditPressed;
 
   String get _timeRange {
     if (isExpiredEmployee) {
@@ -30,21 +29,21 @@ class EmployeeListTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Dismissible(
       background: Container(
-        padding: EdgeInsets.only(left: 16),
+        padding: const EdgeInsets.only(left: 16),
         alignment: Alignment.centerLeft,
         color: Colors.red,
-        child: Icon(Icons.delete_outline_rounded),
+        child: const Icon(Icons.delete_outline_rounded),
       ),
       secondaryBackground: Container(
-        padding: EdgeInsets.only(right: 16),
+        padding: const EdgeInsets.only(right: 16),
         alignment: Alignment.centerRight,
         color: Colors.red,
-        child: Icon(Icons.delete_outline_rounded),
+        child: const Icon(Icons.delete_outline_rounded),
       ),
       onDismissed: (d) => onDeleted.call(),
       key: ObjectKey(employee),
       child: InkWell(
-        onTap: () => _editEmployee(context),
+        onTap: onEditPressed, // () => _editEmployee(context),
         child: Padding(
           padding: const EdgeInsets.all(16),
           child: SizedBox(
@@ -72,10 +71,5 @@ class EmployeeListTile extends StatelessWidget {
         ),
       ),
     );
-  }
-
-  void _editEmployee(BuildContext context) {
-    final qp = AddEditEmployeeScreenArgs.fromEmployee(employee).toJson();
-    context.goNamed(RouteNames.addEmployee, queryParameters: qp);
   }
 }
