63c92555628e4d04a2bf60df5c1e3842772d4b40
Akhil Jose <77378123+akhiljoseko@users.noreply.github.com>
2023-11-08 16:09:29 +0530

    removed provider

--- lib/view/providers/employees_list_provider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-class EmployeesListProvider extends ChangeNotifier {
-  bool get isEmployeesListEmpty => false;
-}
--- lib/view/screens/employee_list_screen.dart
+++ lib/view/screens/employee_list_screen.dart
@@ -4,107 +4,93 @@ import 'package:employee/app/app_icons.dart';
 import 'package:employee/app/app_theme.dart';
 import 'package:employee/app/route_names.dart';
 import 'package:employee/domain/models/employee.dart';
-import 'package:employee/view/providers/employees_list_provider.dart';
 import 'package:employee/view/widgets/employee_list_tile.dart';
-import 'package:employee/view/widgets/empty_list_info.dart';
 import 'package:employee/view/widgets/list_heading_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
 
 class EmployeeListScreen extends StatelessWidget {
   const EmployeeListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => EmployeesListProvider(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Employee List"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(12),
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Employee List"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(12),
           ),
-          onPressed: () => _addEmployee(context),
-          child: SvgPicture.asset(AppIcons.add),
         ),
-        body: Consumer<EmployeesListProvider>(
-          builder: (context, provider, child) {
-            //TODO: Add error info if needed
-            if (provider.isEmployeesListEmpty) {
-              return const EmptyListInfo();
-            }
-            return CustomScrollView(
-              slivers: [
-                SliverList.builder(
-                  itemCount: 7 + 1,
-                  itemBuilder: (context, index) {
-                    if (index == 0) {
-                      return const HeadingText(
-                        headingText: "Current employees",
-                      );
-                    }
-                    final employee = Employee(
-                      id: Random().nextInt(500),
-                      name: "Samantha Lee",
-                      roleId: 2,
-                      fromDate: DateTime(2023, 10, 23),
-                    );
-                    return EmployeeListTile(
-                      key: ValueKey<int>(employee.id!),
-                      employee: employee,
-                      isExpiredEmployee: false,
-                      onDeleted: _deleteEmployee,
-                    );
-                  },
+        onPressed: () => _addEmployee(context),
+        child: SvgPicture.asset(AppIcons.add),
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverList.builder(
+            itemCount: 7 + 1,
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                return const HeadingText(
+                  headingText: "Current employees",
+                );
+              }
+              final employee = Employee(
+                id: Random().nextInt(500),
+                name: "Samantha Lee",
+                roleId: 2,
+                fromDate: DateTime(2023, 10, 23),
+              );
+              return EmployeeListTile(
+                key: ValueKey<int>(employee.id!),
+                employee: employee,
+                isExpiredEmployee: false,
+                onDeleted: _deleteEmployee,
+              );
+            },
+          ),
+          SliverList.builder(
+            itemCount: 5 + 1,
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                return const HeadingText(
+                  headingText: "Previous employees",
+                );
+              }
+              final employee = Employee(
+                id: Random().nextInt(500),
+                name: "James Lee",
+                roleId: 2,
+                fromDate: DateTime(2022, 8, 1),
+                toDate: DateTime(2023, 10, 30),
+              );
+              return EmployeeListTile(
+                key: ValueKey<int>(employee.id!),
+                employee: employee,
+                isExpiredEmployee: true,
+                onDeleted: _deleteEmployee,
+              );
+            },
+          ),
+          SliverToBoxAdapter(
+            child: DecoratedBox(
+              decoration: const BoxDecoration(
+                color: AppColors.grey,
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 24, horizontal: 12),
+                child: Text(
+                  "Swipe left to delete",
+                  style: Theme.of(context).textTheme.labelLarge,
                 ),
-                SliverList.builder(
-                  itemCount: 5 + 1,
-                  itemBuilder: (context, index) {
-                    if (index == 0) {
-                      return const HeadingText(
-                        headingText: "Previous employees",
-                      );
-                    }
-                    final employee = Employee(
-                      id: Random().nextInt(500),
-                      name: "James Lee",
-                      roleId: 2,
-                      fromDate: DateTime(2022, 8, 1),
-                      toDate: DateTime(2023, 10, 30),
-                    );
-                    return EmployeeListTile(
-                      key: ValueKey<int>(employee.id!),
-                      employee: employee,
-                      isExpiredEmployee: true,
-                      onDeleted: _deleteEmployee,
-                    );
-                  },
-                ),
-                SliverToBoxAdapter(
-                  child: DecoratedBox(
-                    decoration: const BoxDecoration(
-                      color: AppColors.grey,
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 24, horizontal: 12),
-                      child: Text(
-                        "Swipe left to delete",
-                        style: Theme.of(context).textTheme.labelLarge,
-                      ),
-                    ),
-                  ),
-                )
-              ],
-            );
-          },
-        ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
