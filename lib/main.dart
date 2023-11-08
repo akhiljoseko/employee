618ae14a4d73963d9b601fd32484fe0aa54f66bf
Akhil Jose <77378123+akhiljoseko@users.noreply.github.com>
2023-11-08 20:13:16 +0530

    di repository

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'package:employee/domain/employee_repository.dart';
+import 'package:employee/domain/employee_repository_impl.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'app/employee_app.dart';
 
 void main() {
   runApp(
-    const EmployeeApp(),
+    MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<EmployeeRepository>(
+          create: (context) => EmployeeRepositoryImpl(),
+        )
+      ],
+      child: const EmployeeApp(),
+    ),
   );
 }
